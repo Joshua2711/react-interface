44335a3f2b2e1c44f12f631ce13a02c0e7d50286
Joshua2711 <joshua_hartshorn@hotmail.co.uk>
2022-05-23 14:59:41 +0100

    Created a toggle function for the drop down sort button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,9 @@
 import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi";
-const DropDown = () => {
+import { useState } from "react";
+const DropDown = ({ toggle }) => {
+  if (!toggle) {
+    return null;
+  }
   return (
     <div
       className="origin-top-right absolute right-0 mt-2 w-56
@@ -46,6 +50,7 @@ const DropDown = () => {
   );
 };
 const Search = () => {
+  let [toggleSort, setToggleSort] = useState(false);
   return (
     <div className="py-5">
       <div className="mt-1 relative rounded-md shadow-sm">
@@ -65,6 +70,9 @@ const Search = () => {
           <div>
             <button
               type="button"
+              onClick={() => {
+                setToggleSort(!toggleSort);
+              }}
               className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center"
               id="options-menu"
               aria-haspopup="true"
@@ -72,7 +80,7 @@ const Search = () => {
             >
               Sort By <BiCaretDown className="ml-2" />
             </button>
-            <DropDown />
+            <DropDown toggle={toggleSort} />
           </div>
         </div>
       </div>
